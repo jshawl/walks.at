0dd6da89afbf7bb731215f98ae897d416c331719
Jesse Shawl <jesse@jesse.sh>
2020-01-15 18:53:49 -0600

    fiss more typohs

--- db/migrate/20200111211534_create_events.rb
+++ db/migrate/20200111211534_create_events.rb
@@ -2,8 +2,8 @@ class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
       t.datetime :created_at
-      t.latitude :float
-      t.longitude :float
+      t.float :latitude
+      t.float :longitude
     end
   end
 end
