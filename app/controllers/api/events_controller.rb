0340ae156be3a14a9eb21c4e1fb47ece06f1d49d
Jesse Shawl <jesse@jesse.sh>
2020-01-15 18:25:20 -0600

    add api endpoint

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  class EventsController < ApplicationController
+    def index
+      params.require([:date_from, :date_to])
+      date_from = Date.parse(params[:date_from]).beginning_of_day
+      date_to = Date.parse(params[:date_to]).end_of_day
+      render json: Event.where(created_at: date_from..date_to)
+    end
+  end
+end
