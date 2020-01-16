f1131f16a1322bc62d81f9693a577c958b4b3c4e
Jesse Shawl <jesse@jesse.sh>
2020-01-15 18:51:27 -0600

    fix typo

--- db/migrate/20200111211534_create_events.rb
+++ db/migrate/20200111211534_create_events.rb
@@ -1,7 +1,7 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
-      t.datettime :created_at
+      t.datetime :created_at
       t.latitude :float
       t.longitude :float
     end
