fab6ba4a647c9fa0bd1e6e3b75815d4d5beb1462
Jesse Shawl <jesse@jesse.sh>
2020-01-16 21:20:36 -0600

    display map

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_15_234904) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.datetime "created_at"
     t.float "latitude"
