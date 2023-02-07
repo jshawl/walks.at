9ab421fecd6688f9d8effbae2758db2c90957c9e
Jesse Shawl <jesse@jesse.sh>
2023-02-07 07:45:17 -0500

    rubocop

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,21 +8,9 @@ class Event < ApplicationRecord
   def self.recent
     yesterday = (Time.now - 1.day).strftime('%F')
     tomorrow = (Time.now + 1.day).strftime('%F')
-    url = "/api/tracks.aspx?key=#{Rails.application.credentials.FOLLOWMEE_API_KEY}&username=#{Rails.application.credentials.FOLLOWMEE_USERNAME}&output=json&function=daterangefordevice&from=#{yesterday}&to=#{tomorrow}&deviceid=#{Rails.application.credentials.FOLLOWMEE_DEVICE_ID}"
-    uri = URI.parse("https://www.followmee.com#{url}")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    res = http.get(uri.request_uri)
-    data = JSON.parse(res.body)['Data']
-    data.each do |datum|
-      create(
-        user: User.first,
-        created_at: datum['Date'],
-        latitude: datum['Latitude'],
-        longitude: datum['Longitude'],
-        speed: datum['Speed(mph)'],
-        direction: datum['Direction'],
-        accuracy: datum['Accuracy']
+    get_followmee_data(yesterday, tomorrow).map do
+      User.first.events.create(
+        formatted_data
       )
     end
   end
@@ -30,8 +18,33 @@ class Event < ApplicationRecord
   def self.between(date_from, date_to)
     where(
       created_at:
-        Date.parse(date_from).beginning_of_day - 21_600..
-        Date.parse(date_to).end_of_day - 21_600
+        (Date.parse(date_from).beginning_of_day - 21_600..
+        Date.parse(date_to).end_of_day - 21_600)
     )
+  end
+
+  private
+
+  def get(url)
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    JSON.parse(http.get(uri.request_uri).body)
+  end
+
+  def get_followmee_data(date_from, date_to)
+    url = "https://www.followmee.com/api/tracks.aspx?key=#{Rails.application.credentials.FOLLOWMEE_API_KEY}&username=#{Rails.application.credentials.FOLLOWMEE_USERNAME}&output=json&function=daterangefordevice&from=#{date_from}&to=#{date_to}&deviceid=#{Rails.application.credentials.FOLLOWMEE_DEVICE_ID}"
+    get(url)['Data']
+  end
+
+  def formatted_data(datum)
+    {
+      created_at: datum['Date'],
+      latitude: datum['Latitude'],
+      longitude: datum['Longitude'],
+      speed: datum['Speed(mph)'],
+      direction: datum['Direction'],
+      accuracy: datum['Accuracy']
+    }
   end
 end
