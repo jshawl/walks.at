42f82ea9afa5df11a5b984f1d7b031ecbb22637a
Jesse Shawl <jesse@jesse.sh>
2023-02-08 08:00:51 -0500

    improve test coverage

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+require 'rails_helper'
+
 describe 'Event' do
+  fixtures :users
   it 'fetches from the api' do
     d = {
       'Date' => '2023-01-01'
@@ -8,5 +11,8 @@ describe 'Event' do
     stub_request(:get, %r{https://www.followmee.com/api/tracks.aspx(.*)})
       .to_return(status: 200, body: "{\"Data\": [#{d.to_json}]}", headers: {})
     expect { Event.recent }.to change { Event.count }.by(1)
+  end
+  it 'has a by_date scope' do
+    Event.by_date(1.hour.ago.strftime('%F'))
   end
 end
