094d215c713dd08f43877c2ee6dc78e30c6c986a
Jesse Shawl <jesse@jesse.sh>
2024-12-22 07:06:33 -0500

    refactor

--- app/javascript/events/explore.js
+++ app/javascript/events/explore.js
@@ -1,26 +1,15 @@
-import { getBounds } from "./application";
+import { fitPlaces } from "application";
 
-const mapInstance = L.map("mapid");
-L.tileLayer(
-  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-  {
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: "mapbox/streets-v11",
-    accessToken:
-      "pk.eyJ1IjoiYW1ibGVhcHAiLCJhIjoiY2s1MXFlc2tmMDBudTNtcDhwYTNlMXF6NCJ9.5sCbcBl56vskuJ2o_e27uQ",
-  }
-).addTo(mapInstance);
-
-pointList = pointList.map(
-  (point) => new L.LatLng(point.latitude, point.longitude)
-);
+let pointList = [...events];
 
 if (env === "development") {
   const drift = () => (Math.random() / 250) * (Math.random() < 0.5 ? -1 : 1);
   for (let i = 0; i < 3; i++) {
     pointList = pointList.reduce((acc, el) => {
-      acc.push({ lat: el.lat + drift(), lng: el.lng + drift() });
+      acc.push({
+        latitude: el.latitude + drift(),
+        longitude: el.longitude + drift(),
+      });
       return acc;
     }, pointList);
   }
@@ -32,18 +21,11 @@ if (bookmark.northeast_latitude) {
     [bookmark.southwest_latitude, bookmark.southwest_longitude],
   ]);
 } else {
-  const bounds = getBounds(pointList);
-  mapInstance.fitBounds([
-    [bounds._northEast.lat, bounds._northEast.lng],
-    [bounds._southWest.lat, bounds._southWest.lng],
-  ]);
+  fitPlaces(pointList);
 }
 
 pointList.map((point) => {
-  if (!point.lat) {
-    throw new Error(JSON.stringify(point));
-  }
-  L.circle([point.lat, point.lng], {
+  L.circle([point.latitude, point.longitude], {
     radius: 50,
     stroke: false,
     fillOpacity: 0.8,
